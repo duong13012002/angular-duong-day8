d08fc4f80d4297368d307306f76dcbb5e65d9a40
duong13012002 <102996301+duong13012002@users.noreply.github.com>
2022-09-09 16:36:37 +0700

    2 files modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { AuthorDetailComponent } from './authors/author-detail.component';
+import { AuthorListComponent } from './authors/author-list.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, AuthorDetailComponent, AuthorListComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/authors/author-list.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { authors,Author } from '../authors';
+
+@Component({
+  selector: 'author-list',
+  template: `
+  <author-details *ngFor = "let author of authors"[author]="author" (select) = "onSelected($event)" (delete) = "onDelete($event)"></author-details>
+  <br>
+  <br>
+  <div>
+    current selected author: {{currentAuthor?.firstName}} {{currentAuthor?.lastName}}
+  </div>
+  <br>
+  `,
+})
+export class AuthorListComponent {
+  authors = authors;
+  currentAuthor = authors[0];
+  onSelected(selectedAuthor:Author){
+    this.currentAuthor = selectedAuthor;
+  }
+  onDelete(id:number){
+    this.authors = this.authors.filter(author =>{
+      return author.id != id;
+    });
+    if(this.currentAuthor.id == id){
+      this.currentAuthor = this.authors[0];
+    }
+  }
+}
